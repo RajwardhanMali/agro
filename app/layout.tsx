67ba5a91a793a613d804d8ee26a4a53f6cb130ed
Soham Kolhatkar <sohamk130405@gmail.com>
2025-03-23 05:15:47 +0530

    added chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import AuthProvider from "@/components/auth-provider";
+import KomunicateChat from "@/components/chatbot";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+          <KomunicateChat />
         </body>
       </html>
     </AuthProvider>
